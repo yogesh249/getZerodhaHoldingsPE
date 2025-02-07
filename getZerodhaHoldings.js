2739f0ab50fc30c9664fb582ba00fa53ae5bac02
yogesh249 <yogesh249@gmail.com>
2025-02-07 10:18:50 +0530

    Added sorting functionality

--- getZerodhaHoldings.js
+++ getZerodhaHoldings.js
@@ -2,17 +2,139 @@ const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
 const app = express();
+const cors = require('cors');
+const PORT = 5000;
+
+// Middleware
+app.use(cors()); // Allows React to call this API
+app.use(express.json());
+
 
 console.log('Starting server...'); // Log when starting the server
+
+app.get("/PE", async (req, res) => {
+    console.log('GET /PE route hit'); // Log when the route is accessed
+    try {
+            let PE = 0;
+            try {
+                const screenerResponse = await axios.get(`https://www.screener.in/company/${req.query.symbol}/consolidated/`);
+                const PEValue = screenerResponse.data.match(/<span class="name">\s*Stock P\/E\s*<\/span>\s*<span class="nowrap value">[^<]*<span class="number">([^<]+)<\/span>/);
+                if (PEValue) {
+                    PE = PEValue[1];
+                    // Remove comma from bookValue String
+                    PE = PE.replace(/,/g, '');
+                    console.log(`PE for ${req.query.symbol}: ${PE}`); // Log the PE value  
+                }
+                else{
+                    console.log(`PE for ${req.query.symbol}: N/A`); // Log the PE value
+                    PE=0;
+                }
+
+
+            } catch (screenerError) {
+                console.error(`Error fetching PE for ${req.query.symbol}:`, screenerError);
+            }
+            res.send(PE);
+        
+    } catch (error) {
+        console.error('Error fetching PE:', error); // Log the error
+        res.status(500).send('Error fetching PE');
+    }
+});
+
+app.get("/BV", async (req, res) => {
+    console.log('GET /BV route hit'); // Log when the route is accessed
+    try {
+            let bookValue = 0;
+            try {
+                const screenerResponse = await axios.get(`https://www.screener.in/company/${req.query.symbol}/consolidated/`);
+                const bookValueMatch = screenerResponse.data.match(/<span class="name">\s*Book Value\s*<\/span>\s*<span class="nowrap value">[^<]*<span class="number">([^<]+)<\/span>/);
+                if (bookValueMatch) {
+                    bookValue = bookValueMatch[1];
+                    // Remove comma from bookValue String
+                    bookValue = bookValue.replace(/,/g, '');
+                    console.log(`bookValue for ${req.query.symbol}: ${bookValue}`); // Log the BV value  
+                }
+                else{
+                    console.log(`bookValue for ${req.query.symbol}: N/A`); // Log the BV value
+                    bookValue=0;
+                }
+
+
+            } catch (screenerError) {
+                console.error(`Error fetching PE for ${req.query.symbol}:`, screenerError);
+            }
+            res.send(bookValue);
+        
+    } catch (error) {
+        console.error('Error fetching BV:', error); // Log the error
+        res.status(500).send('Error fetching BV');
+    }
+});
+app.get("/CMP", async (req, res) => {
+    console.log('GET /BV route hit'); // Log when the route is accessed
+    try {
+            let cmp = 0;
+            try {
+                const screenerResponse = await axios.get(`https://www.screener.in/company/${req.query.symbol}/consolidated/`);
+                const cmpMatch = screenerResponse.data.match(/<span class="name">\s*Current Price\s*<\/span>\s*<span class="nowrap value">[^<]*<span class="number">([^<]+)<\/span>/);
+                if (cmpMatch) {
+                    cmp = cmpMatch[1];
+                    // Remove comma from bookValue String
+                    cmp = cmp.replace(/,/g, '');
+                    console.log(`cmp for ${req.query.symbol}: ${cmp}`); // Log the BV value  
+                }
+                else{
+                    console.log(`cmp for ${req.query.symbol}: N/A`); // Log the BV value
+                    cmp=0;
+                }
+
+
+            } catch (screenerError) {
+                console.error(`Error fetching PE for ${req.query.symbol}:`, screenerError);
+            }
+            res.send(cmp);
+        
+    } catch (error) {
+        console.error('Error fetching BV:', error); // Log the error
+        res.status(500).send('Error fetching cmp');
+    }
+});
+
+app.get("/PB", async (req, res) => {
+    console.log('GET /combined route hit'); // Log when the route is accessed
+    try {
+        const cmpResponse = await axios.get(`http://localhost:3000/CMP?symbol=${req.query.symbol}`);
+        const bvResponse = await axios.get(`http://localhost:3000/BV?symbol=${req.query.symbol}`);
+        if (bvResponse.data != 0) {
+          res.send("" + (cmpResponse.data / bvResponse.data).toFixed(2));
+        } else {
+          res.send(0);
+        }        
+
+    } catch (error) {
+        console.error('Error fetching combined data:', error); // Log the error
+        res.status(500).send('Error fetching combined data');
+    }
+});
+
 
 app.get('/holdings', async (req, res) => {
     console.log('GET /holdings route hit'); // Log when the route is accessed
     try {
         const response = await axios.get('https://kite.zerodha.com/oms/portfolio/holdings', {
-            headers: {
-                'Authorization': 'enctoken dIXXG3TRpPTq/Z/YDc+hfuKkevjsRT1PzRmyRLOML607xPhyabY3kdUk8vOQxOtrXHZDUFWLc9oRqLt8Ub4CYexJF4TTRhh1I00BqMKmIGJusMD+5bdrcA=='
+            // headers: {
+            //     'Authorization': 'enctoken 2scp3+IfaaHaYHh1tI7lpJY1q3bojfnGgiNBjwrfxyqMnfBoT2BIgREcE7bV6d17H3DdOZxaZOq1mST+YQ6G8Zirw7L6jAzl4qqsjPjAxlIwYd3NUqeU7g==',
+            // "Cache-Control": "no-cache"
+            // }
+            headers:
+            {
+                'Authorization': 'enctoken L9/jM121dfZKcepcJiD5tcyzN0hwXD/BJvfJ51EaYbYH42yK4rKWHozkgVY8OolFA/oN8QkgIr7JEuGxA/UltJgAMus2sT8CJcfkYbwDkpQsf6HZlYMeNQ==',
+                "Cache-Control": "no-cache"
             }
         });
+
+
 
         console.log('Received response from API'); // Log when a response is received
 
@@ -33,6 +155,7 @@ app.get('/holdings', async (req, res) => {
                 }
                 th {
                     background-color: #f2f2f2;
+                    cursor: pointer;
                 }
                 tr:nth-child(even) {
                     background-color: #f9f9f9;
@@ -41,20 +164,72 @@ app.get('/holdings', async (req, res) => {
                     background-color: #f1f1f1;
                 }
             </style>
-            <table border="1">
+            <script>
+                function sortTable(n) {
+                    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+                    table = document.getElementById("holdingsTable");
+                    switching = true;
+                    dir = "asc"; 
+                    while (switching) {
+                        switching = false;
+                        rows = table.rows;
+                        for (i = 1; i < (rows.length - 1); i++) {
+                            shouldSwitch = false;
+                            x = rows[i].getElementsByTagName("TD")[n];
+                            y = rows[i + 1].getElementsByTagName("TD")[n];
+                            if (dir == "asc") {
+                                if (!isNaN(parseFloat(x.innerHTML)) && !isNaN(parseFloat(y.innerHTML))) {
+                                    if (parseFloat(x.innerHTML) > parseFloat(y.innerHTML)) {
+                                        shouldSwitch = true;
+                                        break;
+                                    }
+                                } else {
+                                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                                        shouldSwitch = true;
+                                        break;
+                                    }
+                                }
+                            } else if (dir == "desc") {
+                                if (!isNaN(parseFloat(x.innerHTML)) && !isNaN(parseFloat(y.innerHTML))) {
+                                    if (parseFloat(x.innerHTML) < parseFloat(y.innerHTML)) {
+                                        shouldSwitch = true;
+                                        break;
+                                    }
+                                } else {
+                                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                                        shouldSwitch = true;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        if (shouldSwitch) {
+                            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                            switching = true;
+                            switchcount ++;
+                        } else {
+                            if (switchcount == 0 && dir == "asc") {
+                                dir = "desc";
+                                switching = true;
+                            }
+                        }
+                    }
+                }
+            </script>
+            <table id="holdingsTable" border="1">
                 <tr>
-                    <th>Instrument</th>
-                    <th>Book Value</th>
-                     <th>PB</th>
-                     <th>Stock P/E</th>
-                     <th>Grahams Number</th>
-                    <th>Qty.</th>
-                    <th>Avg. cost</th>
-                    <th>LTP</th>
-                    <th>Cur. val</th>
-                    <th>P&L</th>
-                    <th>Net chg.</th>
-                    <th>Day chg.</th>
+                    <th onclick="sortTable(0)">Instrument</th>
+                    <th onclick="sortTable(1)">Book Value</th>
+                     <th onclick="sortTable(2)">PB</th>
+                     <th onclick="sortTable(3)">Stock P/E</th>
+                     <th onclick="sortTable(4)">Grahams Number</th>
+                    <th onclick="sortTable(5)">Qty.</th>
+                    <th onclick="sortTable(6)">Avg. cost</th>
+                    <th onclick="sortTable(7)">LTP</th>
+                    <th onclick="sortTable(8)">Cur. val</th>
+                    <th onclick="sortTable(9)">P&L</th>
+                    <th onclick="sortTable(10)">Net chg.</th>
+                    <th onclick="sortTable(11)">Day chg.</th>
                 </tr>
         `;
 
@@ -103,7 +278,7 @@ app.get('/holdings', async (req, res) => {
                     <td>${bookValue}</td>
                     <td>${PB}</td>
                     <td>${PE}</td>
-                    <td>${PB*PE}</td>
+                    <td>${(PB*PE).toFixed(2)}</td>
                     <td>${holding.quantity}</td>
                     <td>${holding.average_price.toFixed(2)}</td>
                     <td>${holding.last_price.toFixed(2)}</td>
@@ -122,7 +297,6 @@ app.get('/holdings', async (req, res) => {
         res.status(500).send('Error fetching holdings');
     }
 });
-
 app.listen(3000, (error) => {
     if (error) {
         console.error('Error starting server:', error);
